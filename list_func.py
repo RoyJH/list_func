081fc01e1841cb1abfb07d01b7dc5284076c1405
Roy J. Harris III "Trey <Zithlos@gmail.com>
2022-09-24 13:16:19 -0500

    additional functions

--- list_func.py
+++ list_func.py
@@ -1,4 +1,7 @@
 
+from distutils.command.build_scripts import first_line_re
+
+
 lucky_numbers = [4, 8, 15, 16, 23, 42]
 friends = ["Kevin", "Karen", "Jim", "Oscar", "Toby"]
 
@@ -11,6 +14,26 @@ friends.append("Larry")
 #insert - takes two parameters (pos, ent) and insents it into list at designated position
 friends.insert(3, "Michelle")
 
+#clear - clears all elements of list
+#friends.clear()
+
 #remove - removes entitiy from list
 friends.remove("Kevin")
+
+#pop - "pops" last element off of the list
+#friends.pop()
+
+#index - searches list for entity; prints position if found 
+#print(friends.index("Karen"))
+
+#count - counts the number of times entitiy appears in list
+#print(friends.count("ent"))
+
+#reverse - reverses order of entities in list
+#friends.reverse()
+
+#copy - makes a copy of the list
+#friends.copy()
+friends.sort()
+
 print(friends)
